4aa6531ee47cdf08c8f5e7fd36a257ce829f7749
andrey-kupets <andrey.kupets@i.ua>
2021-05-25 01:51:35 +0300

    add port connection

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const { PORT } = require('./config');
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Port ${PORT} is being listened`);
+});
 
 
 
