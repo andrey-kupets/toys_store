134d74296fce153ce8bd7a70994c45eb11c6375f
andrey-kupets <andrey.kupets@i.ua>
2021-05-28 20:36:58 +0300

    mongoose.connect

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const { PORT } = require('./config');
+const { MONGO_URL, PORT } = require('./config');
+
 const app = express();
+
+_connectDB();
 
 app.listen(PORT, () => {
     console.log(`Port ${PORT} is being listened`);
 });
 
+function _connectDB() {
+    mongoose.connect(MONGO_URL);
 
+    const { connection } = mongoose;
+
+    connection.on('error', (error) => {
+        console.log(error);
+    });
+}
+
+
+// app.get('/users', ((req, res) => {
+//     res.json([
+//         {"name": "Vasya"},
+//         {"name": "Petr"},
+//         {"name": "Manya"}
+//     ])
+// }))
 
 
--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
     PORT: 5000,
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost/toys_store',
 };
