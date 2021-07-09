8461ef129ac535f292c10c731060969434964f5e
andrey-kupets <andrey.kupets@i.ua>
2021-07-10 00:36:15 +0300

    add price filter

--- service/product.service.js
+++ service/product.service.js
@@ -2,27 +2,19 @@ const { Product } = require('../models');
 // require('../models'); // SUPER WORK!!
 
 module.exports = {
-  findProducts: (query = {}) => {
-    const { lte, gte, ...filters } = query;
+  findProducts: (query) => {
+    const { priceGte, priceLte, ...filters } = query;
+    console.log(query);
 
-    if (lte) {
-      filters.price = Object.assign({}, filters.price, { $lte: lte });
+    if (priceGte) {
+      filters.price = Object.assign({}, filters.price, { $gte: priceGte });
     }
 
-    if (gte) {
-      filters.price = Object.assign({}, filters.price, { $gte: gte });
+    if (priceLte) {
+      filters.price = Object.assign({}, filters.price, { $lte: priceLte });
     }
 
     return Product.find(filters);
   },
   findProductById: (productId) => Product.findById(productId),
 };
-
-// db.getCollection('teacher').find({
-//   payment: {
-//     $gte: 2500             //  >=
-//     $gt: 2500              //  >
-//     $lte: 2500             //  <=
-//     $lt: 2500              //  <
-//   }
-// })
