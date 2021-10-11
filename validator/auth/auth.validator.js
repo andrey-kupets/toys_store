2d405da644b3a1e0395c0ebde410239e27a921fc
andrey-kupets <andrey.kupets@i.ua>
2021-10-12 00:52:37 +0300

    add Joi-validation middlewares - kicked little imperfection

--- validator/auth/auth.validator.js
+++ validator/auth/auth.validator.js
@@ -2,11 +2,11 @@ const Joi = require('joi');
 
 const { regExpEnum } = require('../../constant');
 
-module.exports = {
+module.exports = Joi.object({
   email: Joi.string()
     .regex(regExpEnum.EMAIL_REGEXP)
     .required(),
   password: Joi.string()
     .regex(regExpEnum.PASSWORD_REGEXP)
     .required()
-};
+});
