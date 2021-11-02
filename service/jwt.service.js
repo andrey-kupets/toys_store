a590c194f85316ae1f1f6eff0f4a1a053c1d8a6b
andrey-kupets <andrey.kupets@i.ua>
2021-11-03 01:10:07 +0300

    jwt.service.js: generate/verify ActionToken

--- service/jwt.service.js
+++ service/jwt.service.js
@@ -5,23 +5,35 @@ const { responseCodesEnum, actionTokensEnum } = require('../constant');
 const { errorMsg, ErrorHandler } = require('../error');
 
 module.exports = {
-  generateActionToken: (tokenType) => {
-    let secretWord = '';
+  generateActionToken: (actionType) => {
+    const secretWord = _getSecretWordForActionToken(actionType);
 
-    switch (tokenType) {
-      case actionTokensEnum.REGISTER_ACTIVATE:
-        secretWord = JWT_ACTION_SECRET;
-        break;
-      case 'forgot_pass': // for ex. in future
-        secretWord = 'forgot_pass_secret';
-        break;
-      default:
-        throw new ErrorHandler(
-          responseCodesEnum.INTERNAL_SERVER_ERROR,
-          errorMsg.WRONG_TOKEN_TYPE
-        );
-    }
+    return jwt.sign({}, secretWord, { expiresIn: '7d' });
+  },
 
-    return jwt.sign({}, secretWord, { expiresIn: '7d' })
+  verifyActionToken: (actionType, token) => {
+    const secretWord = _getSecretWordForActionToken(actionType);
+
+    return jwt.verify(token, secretWord);
   },
 };
+
+function _getSecretWordForActionToken(actionType) {
+  let secretWord = '';
+
+  switch (actionType) {
+    case actionTokensEnum.REGISTER_ACTIVATE:
+      secretWord = JWT_ACTION_SECRET;
+      break;
+    case 'forgot_pass': // for ex. in future
+      secretWord = 'forgot_pass_secret';
+      break;
+    default:
+      throw new ErrorHandler(
+        responseCodesEnum.INTERNAL_SERVER_ERROR,
+        errorMsg.WRONG_TOKEN_TYPE
+      );
+  }
+
+  return secretWord;
+}
