625bc7a7ea7bccba0274234163aa84d5acca2416
andrey-kupets <andrey.kupets@i.ua>
2021-11-03 01:24:22 +0300

    route for register/activate/send

--- router/auth.router.js
+++ router/auth.router.js
@@ -3,9 +3,29 @@ const router = require('express').Router();
 const { authController } = require('../controller');
 const { authMiddleware, userMiddleware } = require('../middleware');
 
-router.post('/', userMiddleware.doesUserExist, authMiddleware.isAuthValid, authController.authUser);
+router.post(
+  '/',
+  userMiddleware.doesUserExist,
+  authMiddleware.isAuthValid,
+  authController.authUser
+);
 
-router.post('/refresh', authMiddleware.checkRefreshToken, authController.refreshToken);
-router.post('/logout', authMiddleware.checkAccessToken, authController.logoutUser);
+router.post(
+  '/refresh',
+  authMiddleware.checkRefreshToken,
+  authController.refreshToken
+);
+
+router.post(
+  '/logout',
+  authMiddleware.checkAccessToken,
+  authController.logoutUser
+);
+
+router.post( // todo controller for activation by email accept
+  '/register/activate/send',
+  authMiddleware.checkRefreshToken,
+  authController.refreshToken
+);
 
 module.exports = router;
